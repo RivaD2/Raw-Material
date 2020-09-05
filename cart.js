ff3ade8a028bb37abe7cad42358d3da56de85136
RivaD2 <rivkadavidowski@gmail.com>
2020-09-05 10:50:00 -0700

    code cleanup

--- cart.js
+++ cart.js
@@ -2,25 +2,6 @@
 
 /*********************** ALL CART RELATED CODE *************************************************/
 
-// Add item to cart (DONE)
-/*Fetch cart items from local storage (DONE)
-      - pull local storage items and add each item to cart
-      - JSON Parse to take items out of local storage
-      - GetItems
-*/
-
-
-// Will need to write method for rendering product in cart (DONE)
-// Remove Items from Cart
-
-/*Add last table row to Cart that has grandtotal (DONE)
-    -will have to do some math here for totals row (keep in mind the cookies project, price * quantity = subtotal)
-    - Add all subtotals to variable
-    - Do all math in the lop where price and quantity exist*/
-
-
-
-
 
 // Global vars
 // created global var for cart so when eventhandler is used to remove items, it can remove them
@@ -38,7 +19,6 @@ function Cart() {
 //cartitem constructor
 // the instance of the New cart item will be included in local storage section (we can save props or instance in LS)
 
-// Used the push method to add new item into cart
 var CartItem = function (name, src, price, quantity) {
   this.name = name;
   this.quantity = quantity;
@@ -50,7 +30,6 @@ var CartItem = function (name, src, price, quantity) {
 // Created a new cart item and then push it into CartItems prop
 // the three params are what is needed to add item to cart
 Cart.prototype.addItem = function(name, src,price, quantity) {
-  console.log('items are added to cart');
   var item = new CartItem(name, src,price, quantity);
   this.cartItems.push(item);
 };
@@ -132,7 +111,6 @@ function initializeCart() {
       var price = parsedProducts[i].price;
       theCart.addItem(name, src, price, quantity);
     }
-    //console.log(parsedProducts);
     console.log(theCart);
     //called renderCart() function here because we need to render cart after we intialize
     // if we remove item from cart, we will have to rerender it
@@ -145,13 +123,11 @@ initializeCart();
 function saveToLocalStorage(productDataArray) {
   var rawMaterialArrayString = JSON.stringify(productDataArray);
   localStorage.setItem('rawMaterials', rawMaterialArrayString);
-  console.log(productDataArray.length + ' items in Cart'); // the number of items in the cart is here
 }
 
 
 function toDollars(amount) {
   var string = '$' + amount + '.00';
-  console.log(string);
   return string;
 }
 
